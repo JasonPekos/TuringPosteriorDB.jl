58cfc3a5b515c163a68452303f1ebf0051644de9
Jason Pekos <78766845+JasonPekos@users.noreply.github.com>
2024-05-27 18:24:26 -0400

    fix accidental =

--- src/models/logearn_height/logearn_height.jl
+++ src/models/logearn_height/logearn_height.jl
@@ -4,7 +4,7 @@ using Turing
     log_earn = log.(earn)
 
     beta ~ filldist(Turing.Flat(), 2)
-    sigma = Turing.FlatPos(0)
+    sigma ~ Turing.FlatPos(0)
 
     log_earn ~ MvNormal(beta[1] .+ beta[2] .* height, sigma^2*I)
 end
